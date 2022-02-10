09f383a905ca62465615bcdd6ae6c952fdccd6d7
Remi Tobias <remitobias1@outlook.com>
2022-02-10 21:48:43 +0000

    More Enemy Ships
    Able to spawn different ships

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,6 +7,7 @@ public class Spawner : MonoBehaviour
     public float MaxRadius = 1f;
     public float Interval = 5f;
     public GameObject ObjToSpawn;
+    public GameObject[] gameObjects;
     private Transform Origin;
 
     private void Awake()
@@ -19,11 +20,21 @@ public class Spawner : MonoBehaviour
         InvokeRepeating("Spawn", 0f, Interval);
     }
 
+    private GameObject GetRandomSpaceship()
+    {
+        int randomNumber = Random.Range(0, gameObjects.Length);
+        GameObject ship = gameObjects[randomNumber];
+        return ship;
+    }
+
     private void Spawn()
     {
         if(Origin == null) { return; }
         Vector3 SpawnPos = Origin.position + Random.onUnitSphere * MaxRadius;
         SpawnPos = new Vector3(SpawnPos.x, 0f, SpawnPos.z);
-        Instantiate(ObjToSpawn, SpawnPos, Quaternion.identity);
+
+        GameObject shipToSpawn = GetRandomSpaceship();
+
+        Instantiate(shipToSpawn, SpawnPos, Quaternion.identity);
     }    
 }
